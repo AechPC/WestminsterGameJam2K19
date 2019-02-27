18ec7d2122db0cad25de8f034429f96316e4eb68
jasninus <jasninus8@hotmail.dk>
2019-02-27 21:22:08 +0100

    Exorcist shoot & reload
    Also made interface for damageable things

---
+++ TeamTheBigBobWestminsterJam2K19/Assets/Scripts/ExorcistGun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExorcistGun : MonoBehaviour
+{
+    [SerializeField] private float shootCooldown, reloadTime, range;
+    private float lastShoot, reloadDiff;
+
+    [SerializeField] private int magSize;
+    private int bulletsLeft;
+
+    [SerializeField] private Transform gunPos;
+
+    [SerializeField] private LayerMask canHit;
+
+    private void Awake()
+    {
+        bulletsLeft = magSize;
+        reloadDiff = reloadTime - shootCooldown;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > lastShoot + shootCooldown && bulletsLeft > 0)
+        {
+            lastShoot = Time.time;
+            bulletsLeft--;
+            RaycastHit2D hit = Physics2D.Raycast(gunPos.transform.position, transform.right, range, canHit);
+            IDamageable target = hit.transform?.GetComponent<IDamageable>();
+            target?.TakeDamage(10);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R)/* && Time.time > lastShoot + shootCooldown*/)
+        {
+            lastShoot = Time.time + reloadDiff;
+            bulletsLeft = magSize;
+        }
+    }
+}
--- TeamTheBigBobWestminsterJam2K19/Assets/Scripts/ExorcistMovement.cs
+++ TeamTheBigBobWestminsterJam2K19/Assets/Scripts/ExorcistMovement.cs
@@ -18,12 +18,14 @@ public class ExorcistMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update () {
         Vector2 movement = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rb.velocity.y);
+	    if (movement.x != 0)
+	        transform.rotation = Quaternion.Euler(0, movement.x > 0 ? 0 : 180, 0);
         rb.velocity = movement;
 
 	    //RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, raycastDis, groundLayers);
      //   Debug.Log(hit.distance);
 
-	    if (Physics2D.Raycast(transform.position, Vector2.down, raycastDis, groundLayers) && Input.GetKeyDown(KeyCode.Space))
+	    if (Physics2D.Raycast(transform.position, Vector2.down, raycastDis, groundLayers) && Input.GetKeyDown(KeyCode.W))
 	    {
 	        rb.AddForce(new Vector2(0, jumpForce));
 	    }
