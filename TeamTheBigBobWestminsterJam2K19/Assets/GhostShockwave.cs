373ccf03e427119b12745249f0ee4a83465212f3
jasninus <jasninus8@hotmail.dk>
2019-02-27 20:24:52 +0100

    Add shockwave

---
+++ TeamTheBigBobWestminsterJam2K19/Assets/GhostShockwave.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GhostShockwave : MonoBehaviour
+{
+    [SerializeField] private float cooldown, radius, pushForce;
+    private float lastUse;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O) && Time.time > lastUse + cooldown)
+        {
+            Debug.Log("BLASTING!");
+            lastUse = Time.time;
+
+            Rigidbody2D[] inRange = Physics2D.OverlapCircleAll(transform.position, radius).Select(c => c.GetComponent<Rigidbody2D>()).ToArray();
+            foreach (Rigidbody2D rigidbody in inRange)
+            {
+                if (rigidbody)
+                {
+                    Vector2 direction = (rigidbody.transform.position - transform.position).normalized;
+                    rigidbody.AddForce(direction * (radius - Vector2.Distance(transform.position, rigidbody.transform.position)) * pushForce);
+                }
+            }
+        }
+    }
+}
--- TeamTheBigBobWestminsterJam2K19/Assets/Platform.cs
+++ TeamTheBigBobWestminsterJam2K19/Assets/Platform.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Axis
+{
+    Horizontal,
+    Vertical
+}
+
 public class Platform : MonoBehaviour
 {
+    [SerializeField] private Axis axis;
+
     public void Move(Vector2 movement)
     {
-        transform.Translate(movement);
+        Vector2 translation = axis == Axis.Horizontal ? new Vector2(movement.x, 0) : new Vector2(0, movement.y);
+        transform.Translate(translation);
     }
 }
