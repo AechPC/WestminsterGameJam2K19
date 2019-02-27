9afb41eeca903cbe93b6f76acff2b6be129140ba
jasninus <jasninus8@hotmail.dk>
2019-02-27 22:42:20 +0100

    Exorcist shockwave

---
+++ TeamTheBigBobWestminsterJam2K19/Assets/Scripts/ExorcistShockwave.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExorcistShockwave : MonoBehaviour
+{
+    [SerializeField] private float cooldown;
+    private float lastUsed;
+
+    [SerializeField] private int damage;
+
+    [SerializeField] private Vector2 dimensions;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H) && Time.time > lastUsed + cooldown)
+        {
+            lastUsed = Time.time;
+            Vector2 lowerCorner = new Vector2(transform.position.x + (transform.rotation.eulerAngles.y < 90 ? transform.localScale.x: -transform.localScale.x), transform.position.y - transform.localScale.y / 2);
+            Collider2D[] hit = Physics2D.OverlapAreaAll(lowerCorner, lowerCorner + new Vector2(transform.rotation.eulerAngles.y < 90 ? dimensions.x : -dimensions.x, dimensions.y));
+            foreach (Collider2D coll in hit)
+            {
+                IDamageable damageable = coll.GetComponent<IDamageable>();
+                ISpecialDestroy specialDestroy = coll.GetComponent<ISpecialDestroy>();
+                damageable?.TakeDamage(damage);
+                specialDestroy?.SpecialDestroy();
+            }
+        }
+    }
+}
---
+++ TeamTheBigBobWestminsterJam2K19/Assets/Scripts/TestSpecialDestroyable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpecialDestroyable : MonoBehaviour, ISpecialDestroy {
+    public void SpecialDestroy()
+    {
+        Debug.Log("I was specially destroyed");
+    }
+}
