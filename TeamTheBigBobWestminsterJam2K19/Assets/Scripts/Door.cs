1c31e61ed5fb066e41d61d1a12407908c7861819
AechPC <phillip@filtair.dk>
2019-02-28 15:41:35 +0000

    Added ghost lever

---
+++ TeamTheBigBobWestminsterJam2K19/Assets/Scripts/Door.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private Collider2D collisionBox;
+
+    private void Start()
+    {
+        collisionBox = GetComponent<BoxCollider2D>();
+    }
+
+    public void OpenDoor()
+    {
+        collisionBox.isTrigger = true;
+    }
+}
