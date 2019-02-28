92406a6b1ee005a596f8ad2eb8ce3a6aeb138fd8
jasninus <jasninus8@hotmail.dk>
2019-02-28 10:09:50 +0100

    Shield guy done
    Exorcist detection, movement, damage, health, dying

---
+++ TeamTheBigBobWestminsterJam2K19/Assets/Scripts/ShieldEnemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldEnemy : MonoBehaviour, IDamageable
+{
+    [SerializeField] private LayerMask exorcistLayer;
+
+    [SerializeField] private float sightRange, movementSpeed;
+
+    [SerializeField] private int health, damageDealt;
+
+    private Transform exorcistTransform;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        exorcistTransform = GameObject.FindWithTag("Exorcist").transform;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics2D.Raycast(transform.position, Vector2.left, sightRange, exorcistLayer)) // Look left
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+
+            rb.velocity = new Vector2(-movementSpeed, rb.velocity.y);
+        }
+        else if (Physics2D.Raycast(transform.position, Vector2.right, sightRange, exorcistLayer)) // Look right
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+
+            rb.velocity = new Vector2(movementSpeed, rb.velocity.y);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health < 1)
+        {
+            Debug.Log("Shield guy down");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Exorcist")
+        {
+            other.gameObject.GetComponent<IDamageable>().TakeDamage(damageDealt);
+        }
+    }
+}
