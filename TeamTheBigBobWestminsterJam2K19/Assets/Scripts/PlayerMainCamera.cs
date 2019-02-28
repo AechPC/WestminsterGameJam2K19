60d32f772c487e4a6f0e24c2f28b1150c2b1fe13
DarudE <w1682416@my.westminster.ac.uk>
2019-02-28 14:22:35 +0000

    Camera Mechanic for ghost tested/working

---
+++ TeamTheBigBobWestminsterJam2K19/Assets/Scripts/PlayerMainCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMainCamera : MonoBehaviour {
+
+	[SerializeField]
+	private float maximumX;
+	[SerializeField]
+	private float maximumY;
+	[SerializeField]
+	private float minimumX;
+	[SerializeField]
+	private float minimumY;
+
+	private Transform target;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindWithTag("Exorcist").transform;
+	
+	}
+
+	// Update is called once per frame
+	void Update() {
+		transform.position = new Vector3(target.position.x, target.position.y + 3.3f, -10);
+	}
+}
